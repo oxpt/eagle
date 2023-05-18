d75e47feba50203b988353d8faedc569b2c486ed
Ryo Hirayama <ryo12redstone@gmail.com>
2023-05-19 06:45:33 +0900

    Add files

---
+++ tools/check-ci.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+# fail fast
+set -eo pipefail
+shopt -s inherit_errexit
+
+for OPT in "$@"
+do
+    case $OPT in
+        --no-cargo-deny)
+	    NO_CARGO_DENY=1
+            ;;
+    esac
+done
+
+tools/diff-crates.sh
+[[ -z ${NO_CARGO_DENY} ]] && cargo deny check --config configs/deny.toml
+cargo fmt --all -- --check
+cargo clippy --all-targets --all-features -- -D warnings -W clippy::all -W clippy::dbg_macro
+cargo check --tests --all-features
+cargo test --no-run --locked --all-features
+cargo test --all-features
