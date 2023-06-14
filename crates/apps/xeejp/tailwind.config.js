ca101c80ee11f5616f4d5ef4a48f4a0c3393eb75
Ryohei Hayashi <pey@ryohei.info>
2023-06-15 03:00:08 +0900

    Add create_room page

--- crates/apps/xeejp/tailwind.config.js
+++ crates/apps/xeejp/tailwind.config.js
@@ -1,7 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -9,40 +6,7 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        indigo: {
-          50: '#e8eaf6',
-          100: '#c5cae9',
-          200: '#9fa8da',
-          300: '#7986cb',
-          400: '#5c6bc0',
-          500: '#3f51b5',
-          600: '#3949ab',
-          700: '#303f9f',
-          800: '#283593',
-          900: '#1a237e',
-        },
-        pink: {
-          50: '#fce4ec',
-          100: '#f8bbd0',
-          200: '#f48fb1',
-          300: '#f06292',
-          400: '#ec407a',
-          500: '#e91e63',
-          600: '#d81b60',
-          700: '#c2185b',
-          800: '#ad1457',
-          900: '#880e4f',
-        },
-      },
-      fontFamily: {
-        sans: ['var(--font-gothic)', ...defaultTheme.fontFamily.sans],
-        serif: ['var(--font-mincho)', ...defaultTheme.fontFamily.serif],
-        'sans-bold': ['var(--font-gothic-bold)', ...defaultTheme.fontFamily.sans],
-        'serif-bold': ['var(--font-mincho-bold)', ...defaultTheme.fontFamily.serif],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
