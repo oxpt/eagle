4a125dac4c084fe388c0d8ed0bcc4f15af3db05e
Ryohei Hayashi <pey@ryohei.info>
2023-06-14 21:43:01 +0900

    Configure ESLint and prettier

--- crates/apps/xeejp/tailwind.config.js
+++ crates/apps/xeejp/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +10,39 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        indigo: {
+          50: '#e8eaf6',
+          100: '#c5cae9',
+          200: '#9fa8da',
+          300: '#7986cb',
+          400: '#5c6bc0',
+          500: '#3f51b5',
+          600: '#3949ab',
+          700: '#303f9f',
+          800: '#283593',
+          900: '#1a237e',
+        },
+        pink: {
+          50: '#fce4ec',
+          100: '#f8bbd0',
+          200: '#f48fb1',
+          300: '#f06292',
+          400: '#ec407a',
+          500: '#e91e63',
+          600: '#d81b60',
+          700: '#c2185b',
+          800: '#ad1457',
+          900: '#880e4f',
+        },
+      },
+      fontFamily: {
+        sans: ['var(--font-gothic)', ...defaultTheme.fontFamily.sans],
+        serif: ['var(--font-mincho)', ...defaultTheme.fontFamily.serif],
+        'sans-bold': ['var(--font-gothic-bold)', ...defaultTheme.fontFamily.sans],
+        'serif-bold': ['var(--font-mincho-bold)', ...defaultTheme.fontFamily.serif],
       },
     },
   },
   plugins: [],
-}
+};
