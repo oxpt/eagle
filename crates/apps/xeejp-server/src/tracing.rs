7a47d43593b3ab1b945d86d1db2650ef8a5e3438
Ryo Hirayama <ryo12redstone@gmail.com>
2023-11-05 15:40:26 +0900

    Use UtcTime with the wasm feature

--- crates/apps/xeejp-server/src/tracing.rs
+++ crates/apps/xeejp-server/src/tracing.rs
@@ -1,5 +1,7 @@
 use tracing_subscriber::{
-    fmt::format::Pretty, prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt,
+    fmt::{format::Pretty, time::UtcTime},
+    prelude::__tracing_subscriber_SubscriberExt,
+    util::SubscriberInitExt,
 };
 use tracing_web::{performance_layer, MakeConsoleWriter};
 
@@ -7,7 +9,7 @@ pub fn init_tracing_once() {
     let fmt_layer = tracing_subscriber::fmt::layer()
         .json()
         .with_ansi(false) // Only partially supported across JavaScript runtimes
-        .without_time()
+        .with_timer(UtcTime::rfc_3339()) // std::time is not available in browsers
         .with_writer(MakeConsoleWriter); // write events to the console
     let perf_layer = performance_layer().with_details_from_fields(Pretty::default());
     let _ = tracing_subscriber::registry()
